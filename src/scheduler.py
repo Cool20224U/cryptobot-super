bb0d646ec58c664fdef39a4b52ae4d7f60bd511e
Cool20224U <107519594+Cool20224U@users.noreply.github.com>
2026-03-22 22:17:32 +0400

    Initialize BackgroundScheduler in scheduler.py

---
+++ src/scheduler.py
@@ -0,0 +1,5 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+# daily_report, weekly_learning, quarterly_burniske, monthly_real_yield, self_audit_email
+scheduler = BackgroundScheduler()
+scheduler.start()
